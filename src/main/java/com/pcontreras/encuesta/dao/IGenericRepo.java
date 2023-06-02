efeeec74499435e5a2c342ce07d4cca047d4f0fe
patorma <pocontreras@ing.ucsc.cl>
2023-06-01 22:44:14 -0400

    se crea una interface generica para que las demas interface asociadas a las entidades hereden los metodos de esta interface generica

--- src/main/java/com/pcontreras/encuesta/dao/IGenericRepo.java
+++ src/main/java/com/pcontreras/encuesta/dao/IGenericRepo.java
@@ -1,5 +1,11 @@
 package com.pcontreras.encuesta.dao;
 
-public interface IGenericRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+//Se crea interfaz generica para hacer uso de los métodos de JpaRewpository por parte de las inreface relacionadas 
+//a las entidades
+@NoRepositoryBean
+public interface IGenericRepo<T,ID> extends JpaRepository<T, ID> {
 
 }
