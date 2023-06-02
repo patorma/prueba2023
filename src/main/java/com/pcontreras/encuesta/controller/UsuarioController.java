da7fb2dc99d04a4cd5f332e5831a275548fd85a2
patorma <pocontreras@ing.ucsc.cl>
2023-06-02 19:57:53 -0400

    Se termino con el cotroller del usuario

--- src/main/java/com/pcontreras/encuesta/controller/UsuarioController.java
+++ src/main/java/com/pcontreras/encuesta/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.pcontreras.encuesta.controller;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -11,14 +12,21 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pcontreras.encuesta.model.Usuario;
 import com.pcontreras.encuesta.service.IUsuarioService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:4200" })
@@ -59,5 +67,101 @@ public class UsuarioController {
 		}
 		
 		return new ResponseEntity<Usuario>(usuario,HttpStatus.OK);
+	}
+	
+	@PostMapping("/usuarios")
+	public ResponseEntity<?> create(@Valid @RequestBody Usuario usuario,BindingResult result) throws Exception{
+		// es el nuevo usuario creado
+		//se inicializa
+		Usuario usuarioNew = null;
+		Map<String, Object> response =new HashMap<>();
+		
+		// se valida si contiene errores el objeto 
+		if(result.hasErrors()) {
+			
+			List<String> errors = result.getFieldErrors()
+					     .stream()
+					     .map(err -> "El campo '"+ err.getField() + "' "+err.getDefaultMessage())
+					     .collect(Collectors.toList());
+			response.put("errors", errors);
+			return new ResponseEntity<Map<String,Object>>(response,HttpStatus.BAD_REQUEST);
+		}
+		try {
+			usuarioNew = usuarioService.registrar(usuario);
+		}catch(DataAccessException e) {
+			response.put("mensaje", "Error al realizar el insert en la base de datos!");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("mensaje", "El usuario ha sido creado éxito! ");
+		response.put("usuario", usuarioNew);
+		return new ResponseEntity<Map<String, Object>>(response,HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/usuarios/{id}")
+	public ResponseEntity<?> update(@Valid @RequestBody Usuario usuario,BindingResult result,@PathVariable Long id) throws Exception{
+		
+		//se obtiene el usuario que se quiere modificar
+		Usuario usuarioActual = usuarioService.listarPorId(id);
+		
+		//Usuario ya actualizado
+		Usuario usuarioUpdated = null;
+		
+
+		Map<String, Object> response = new HashMap<>();
+		
+		if(result.hasErrors()) {
+			// se debe obtener los mensajes de errror de cada campo 
+			// y convertir estos en una lista de errores de tipo string
+			
+			// se debe convertir esta lista de fielderrors en String
+			List<String> errors = result.getFieldErrors()
+					.stream()
+					.map(err -> "El campo '"+ err.getField() + "' "+err.getDefaultMessage())// muy parecido  al operador map en angular (rxjs), mismo concepto!
+					.collect(Collectors.toList());// ahora podemos convertir de regreso el stream  aun tipo List
+			response.put("errors", errors);
+			// se responde con un responseentity con listados de error
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.BAD_REQUEST);
+			
+			// en lo anterior se recibe un field errors y lo convertimos a string
+		}
+		
+		if(usuarioActual == null) {
+			response.put("mensaje", "Error: no se pudo editar, elusuario con ID: ".concat(id.toString().concat(" no existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.NOT_FOUND);
+		}
+		
+		try {
+			//modificamos los datos del usuario actual con los datos del gasto que te envien
+			usuarioActual.setMail(usuario.getMail());
+			usuarioActual.setTipoMusica(usuario.getTipoMusica());
+			
+			usuarioUpdated = usuarioService.registrar(usuarioActual);
+		}catch(DataAccessException e) {
+			response.put("mensaje", "Error al actualizar al usuario en la base de datos!");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}	
+		
+		response.put("mensaje", "El usuario ha sido actualizado con éxito!");
+		response.put("usuario", usuarioUpdated);
+		
+		return new ResponseEntity<Map<String,Object>>(response,HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("/usuarios/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) throws Exception{
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+			usuarioService.eliminar(id);
+		}catch (DataAccessException e) {
+			response.put("mensaje", "Error al eliminar al usuario de la base de datos!");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("mensaje", "El usuario fue eliminado con éxito!");
+		return new ResponseEntity<Map<String, Object>>(response,HttpStatus.OK);
 	}
 }
